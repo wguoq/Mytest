85807b97c2ad5cdb639e1838f248d2c7aa0da20e
wanggq <wgq1218@gmail.com>
2015-12-04 18:18:38 +0800

    第一个case运行通过

--- Test_release.py
+++ Test_release.py
@@ -2,19 +2,25 @@
 from selenium import webdriver
 import Page_script
 import re
+import tools
 
 
 def initialize(driver, config):
-
-    Page_script.initialize()
     print('D1_initialize')
+    url = 'http://'+config.get('default_ip')
+    password = config.get('admin_pw')
+    Page_script.initialize(driver, url, password)
 
 
-def login():
+def login(driver, config):
     print('D1_login')
+    url = 'http://'+config.get('default_ip')
+    password = config.get('admin_pw')
+    Page_script.open_url(driver, url)
+    Page_script.login(driver, password)
 
 
-def pppoe():
+def pppoe(driver, config):
     print('D1_pppoe')
 
 
@@ -51,19 +57,19 @@ def ck_oder(test_case):
     return 1
 
 if __name__ == '__main__':
-    # chrome = webdriver.Chrome()
-    test_url = 'http://192.168.99.1'
-    pw = '12345678'
+    with open('testconfig.ini', 'r', encoding='utf-8') as f:
+        conf = tools.get_config(f.readlines())
     with open('Testlist.ini', 'r', encoding='utf-8') as f:
         test_lst = f.readlines()
 
     ts_case = get_case(test_lst)
+
     if ck_format(ts_case) & ck_oder(ts_case) == 1:
-        #print(ts_case)
+        chrome = webdriver.Chrome()
         for ts in ts_case:
-            print(ts[1])
             if ts[1] in D1:
-                D1.get(ts[1])()
+                D1.get(ts[1])(chrome, conf)
+        chrome.close()
     else:
         print('22222')
 
