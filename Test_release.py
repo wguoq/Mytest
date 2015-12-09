d175a1d79ab43ecdeaded0bd495da6d71bb2d284
wanggq <wgq1218@gmail.com>
2015-12-09 17:47:27 +0800

    Merge branch 'master' of D:\DTtest with conflicts.

--- Test_release.py
+++ Test_release.py
@@ -86,20 +86,19 @@ def ck_oder(test_case):
 
 
 if __name__ == '__main__':
+    chrome = webdriver.Chrome()
     with open('testconfig.ini', 'r', encoding='utf-8') as f:
         conf = tools.get_config(f.readlines())
-    with open('Testlist.ini', 'r', encoding='utf-8') as f:
-        test_lst = f.readlines()
-    chrome = webdriver.Chrome()
-    admin_pw = conf.get('admin_pw')
-    default_url = 'http://'+conf.get('default_ip')
+        admin_pw = conf.get('admin_pw')
+        default_url = 'http://'+conf.get('default_ip')
 
-    #{testname:(func,[params])} testname必须和配置文件里面一样
+    with open('Testlist.ini', 'r', encoding='utf-8') as f:
+        ts_case = get_case(f.readlines())
+
     script = {'D1_initialize': (init, [chrome, default_url, conf]),
               'D1_login': (login, [chrome, default_url, admin_pw]),
               'D1_pppoe': (pppoe, [chrome, conf])}
 
-    ts_case = get_case(test_lst)
     if ck_format(ts_case) & ck_oder(ts_case) == 1:
         build = []
         for t in ts_case:
@@ -107,9 +106,7 @@ if __name__ == '__main__':
                 build.append(script.get(t[1]))
         for func, param in build:
             func(*param)
-        chrome.close()
     else:
         print('22222')
-
-
+    chrome.close()
 
