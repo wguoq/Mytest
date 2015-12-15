820f355490c7ec7828ab2efb001f2b762c8be211
wanggq <wgq1218@gmail.com>
2015-12-15 14:51:32 +0800

    初始化配置支持拨号

--- Test_reset.py
+++ Test_reset.py
@@ -25,61 +25,15 @@ logging.getLogger('').addHandler(console)
 #################################################################################################
 
 
-def detect_wan(driver):
-    flag = ['运营商', 'DHCP']
-    try:
-        tip = str(driver.find_element_by_css_selector("p.tips-text").text)
-        for a in flag:
-            if tip.find(a) > -1:
-                return a
-    except Exception as e:
-        print(e)
-        return 0
-
-
-def initialize(driver, url):
-    try:
-        time.sleep(3)
-        driver.get(url)
-        time.sleep(3)
-        driver.find_element_by_id("init-protocol-checkbox").click()
-        time.sleep(3)
-        driver.find_element_by_id("initalize").click()
-        time.sleep(30)
-        wan = detect_wan(driver)
-        if wan == 0:
-            logging.info('wan = 跳过检测')
-            driver.find_element_by_link_text(u"跳过检测").click()
-        else:
-            if 'DHCP' == wan:
-                logging.info('wan = '+wan)
-                driver.find_element_by_link_text(u"下一步").click()
-                time.sleep(10)
-            if '运营商' == wan:
-                logging.info('wan = '+wan)
-                #TODO
-        time.sleep(3)
-        driver.find_element_by_id("key").clear()
-        time.sleep(3)
-        driver.find_element_by_id("key").send_keys("12345678")
-        time.sleep(3)
-        driver.find_element_by_id("wifi").click()
-        time.sleep(10)
-        driver.find_element_by_link_text(u"登录路由器").click()
-        time.sleep(5)
-        return 1
-    except Exception as e:
-        print(e)
-        return 0
-
-
-def do_test(driver, url):
-    initialize(driver, url)
+def do_test(driver, url, password, config):
+    pppoe_user = config.get('pppoe_user')
+    pppoe_pwd = config.get('pppoe_pwd')
+    Page_script.initialize(driver, url, password, username=pppoe_user, pw=pppoe_pwd)
     if Page_script.open_url(driver, url) == 1:
         time.sleep(3)
     else:
         return 0
-    if Page_script.login(driver, pw) == 1:
+    if Page_script.login(driver, admin_pw) == 1:
         time.sleep(3)
     else:
         return 0
@@ -91,12 +45,12 @@ def do_test(driver, url):
         time.sleep(3)
     else:
         return 0
-    initialize(driver, url)
+    Page_script.initialize(driver, url, password, username=pppoe_user, pw=pppoe_pwd)
     if Page_script.open_url(driver, url) == 1:
         time.sleep(3)
     else:
         return 0
-    if Page_script.login(driver, pw) == 1:
+    if Page_script.login(driver, admin_pw) == 1:
         time.sleep(2)
     else:
         return 0
@@ -119,7 +73,7 @@ if __name__ == '__main__':
     num = int(conf.get("reset_times"))
     test_ip = conf.get("reset_ip")
     test_url = 'http://' + test_ip
-    pw = conf.get("admin_pw")
+    admin_pw = conf.get("admin_pw")
     new_ssid = conf.get("new_ssid")
     old_5ssid = conf.get("default_5ssid")
     wait_time = int(conf.get("wait_time1"))
@@ -128,7 +82,7 @@ if __name__ == '__main__':
         logging.info('====run test==== %s', i+1)
         chrome = webdriver.Chrome()
         if tools.ping_ok(test_ip) == 1:
-            if do_test(chrome, test_url) == 1:
+            if do_test(chrome, test_url, admin_pw, conf) == 1:
                 chrome.quit()
             else:
                 fail += 1
